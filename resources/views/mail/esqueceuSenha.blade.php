f46fdb78597668fe94aec08807f7735a01dd0ce6
ajaax1 <ruanhigor123@gmail.com>
2024-07-17 19:44:08 -0300

    segundo

--- resources/views/mail/esqueceuSenha.blade.php
+++ resources/views/mail/esqueceuSenha.blade.php
@@ -4,7 +4,7 @@
             Recebemos uma solicitação para redefinir a senha da sua conta.
             Para redefinir sua senha, clique no botão abaixo.
         </p>
-        <a href="http://localhost:5173/mudar-senha/{{$token}}" target="_blank">Redefinir senha</a>
+        <a href="https://front-tarefas-ja.vercel.app/home/{{$token}}" target="_blank">Redefinir senha</a>
         <p>
             Se você não solicitou uma redefinição de senha, ignore este e-mail.
         </p>
